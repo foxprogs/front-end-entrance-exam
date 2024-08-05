85869b6d0603a2fe47f61beff6e098524206215c
Vitek Foxprogs <foxprogs@gmail.com>
2024-08-05 11:07:16 +0400

    remove comments, fix w3 validator warnings

--- js/main.js
+++ js/main.js
@@ -16,23 +16,18 @@ document.querySelectorAll("[contenteditable]").forEach((element) => {
     const ripple = document.createElement("span");
     ripple.classList.add("ripple-effect");
 
-    // Добавляем элемент волны в контейнер
     this.appendChild(ripple);
 
-    // Получаем размеры и позицию контейнера
     const rect = this.getBoundingClientRect();
     const size = Math.max(rect.width, rect.height);
 
-    // Позиция клика относительно контейнера
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    // Устанавливаем размеры и позицию волны
     ripple.style.width = ripple.style.height = `${size}px`;
     ripple.style.left = `${x - size / 2}px`;
     ripple.style.top = `${y - size / 2}px`;
 
-    // Удаляем волну после завершения анимации
     ripple.addEventListener("animationend", () => ripple.remove());
   });
 });
